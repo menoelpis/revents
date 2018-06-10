f128a8870667adf1c3b39c914684cb1018f42267
Daniel Park <abideinhope@gmail.com>
2018-06-11 08:02:50 +0900

    Fixed NavBar and Main Component Overlap Issue

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
 
@@ -7,7 +8,9 @@ class App extends Component {
     return (
       <div className='app'>
         <NavBar />
-        <EventDashboard />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
