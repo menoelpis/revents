cb51ad9687b6f29b4e445bf56869f48c7ce43e8d
Daniel Park <abideinhope@gmail.com>
2018-06-11 17:20:02 +0900

    Imported Components into App.jsx

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
 import { Route } from 'react-router-dom';
-import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import EventForm from '../../features/event/EventForm/EventForm';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
 
 class App extends Component {
   render() {
@@ -10,7 +15,13 @@ class App extends Component {
       <div className='app'>
         <NavBar />
         <Container className="main">
+          <Route path='/' component={EventDashboard} />
           <Route path='/events' component={EventDashboard} />
+          <Route path='/event/:id' component={EventDetailedPage} />
+          <Route path='/people' component={PeopleDashboard} />
+          <Route path='/profile/:id' component={UserDetailedPage} />
+          <Route path='/settings' component={SettingsDashboard} />
+          <Route path='/createEvent' component={EventForm} />
         </Container>
       </div>
     );
