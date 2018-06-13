81c275bb8693de6fb24a79c993308b7e33f4ff26
Daniel Park <abideinhope@gmail.com>
2018-06-13 11:49:15 +0900

    Implemented SignIn and SignOut Mockup Logic with Some errors

--- src/features/nav/Menus/SignedOutMenu.jsx
+++ src/features/nav/Menus/SignedOutMenu.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Menu, Button } from 'semantic-ui-react';
 
-const SignedOutMenu = () {
+const SignedOutMenu = ({signIn}) =>  {
   return (
-    <div>
-    
-    </div>
+    <Menu.Item position="right">
+      <Button onClick={signIn} basic inverted content="Login" />
+      <Button basic inverted content="Register" style={{ marginLeft: '0.5em'}} />
+    </Menu.Item>
   );
 }
 
