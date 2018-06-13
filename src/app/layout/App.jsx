f123d3e8110bb030b5fa521cc336913440133986
Daniel Park <abideinhope@gmail.com>
2018-06-13 10:30:33 +0900

    Added Switch Component to Divide Home Page with Rest Route

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import NavBar from '../../features/nav/NavBar/NavBar';
 import EventForm from '../../features/event/EventForm/EventForm';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
@@ -13,18 +13,27 @@ import HomePage from '../../features/home/HomePage';
 class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <NavBar />
-        <Container className="main">
+      <div>
+        <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/events' component={EventDashboard} />
-          <Route path='/event/:id' component={EventDetailedPage} />
-          <Route path='/people' component={PeopleDashboard} />
-          <Route path='/profile/:id' component={UserDetailedPage} />
-          <Route path='/settings' component={SettingsDashboard} />
-          <Route path='/createEvent' component={EventForm} />
-        </Container>
-      </div>
+        </Switch>
+
+        <Route path="/(.+)" render={() => (
+          <div>
+            <NavBar />
+            <Container className="main">
+              <Switch>
+                <Route path='/events' component={EventDashboard} />
+                <Route path='/event/:id' component={EventDetailedPage} />
+                <Route path='/people' component={PeopleDashboard} />
+                <Route path='/profile/:id' component={UserDetailedPage} />
+                <Route path='/settings' component={SettingsDashboard} />
+                <Route path='/createEvent' component={EventForm} />
+              </Switch>
+            </Container>
+          </div>
+        )}/>
+    </div>
     );
   }
 }
